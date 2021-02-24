a44422dacd7c87da9bdc91eddca176d57774df77
Chaosmaster <chaosmaster@chaosv.de>
2021-02-24 17:28:24 +0100

    add boot-recovery and boot-fastboot to dist

--- makedist.sh
+++ makedist.sh
@@ -9,7 +9,7 @@ mkdir -p dist/unlock/kamakiri/lk-payload/build
 cp lk-payload/build/payload.bin dist/unlock/kamakiri/lk-payload/build/
 
 mkdir -p dist/unlock/kamakiri/modules
-cp modules/{common.py,load_payload.py,logger.py,main.py,functions.py} dist/unlock/kamakiri/modules/
+cp modules/{common.py,load_payload.py,logger.py,main.py,functions.py,handshake2.py} dist/unlock/kamakiri/modules/
 
 mkdir -p dist/unlock/kamakiri/brom-payload/stage1
 cp brom-payload/stage1/stage1.bin dist/unlock/kamakiri/brom-payload/stage1/
@@ -17,7 +17,7 @@ cp brom-payload/stage1/stage1.bin dist/unlock/kamakiri/brom-payload/stage1/
 mkdir -p dist/unlock/kamakiri/brom-payload/stage2
 cp brom-payload/stage2/stage2.bin dist/unlock/kamakiri/brom-payload/stage2/
 
-cp {bootrom-step.sh,fastboot-step.sh} dist/unlock/kamakiri/
+cp {bootrom-step.sh,fastboot-step.sh,boot-recovery.sh,boot-fastboot.sh} dist/unlock/kamakiri/
 
 mkdir -p dist/unlock/META-INF/com/google/android
 cp META-INF/com/google/android/{update-binary,updater-script} dist/unlock/META-INF/com/google/android/
